5345ab1efb70a1441a32853439ec3b6f130d12bc
surbhi-sharma1 <surbhi.sharma1@sourcefuse.com>
2022-06-21 15:04:14 +0530

    Initial commit

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  BASE_URL: 'http://localhost:3000',
+  PUBLISH_KEY: 'pub-c-d6b50755-2212-48ec-99da-38ecc57c31d4',
+  SUBSCRIBE_KEY: 'sub-c-289ccd94-4b30-4717-bd00-d75e10f07f10',
+  CHAT_ROOM: 'ea24d84f-ef69-403c-b6c4-7758ea9c2093',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
